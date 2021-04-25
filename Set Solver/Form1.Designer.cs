1575a1d100c9944582973154cb0e0039432a1ab3
Laurens <laurensmeijerink@gmail.com>
2021-04-25 17:45:08 +0200

    Found some more stuff to rename

--- Set Solver/Form1.Designer.cs
+++ Set Solver/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ShapeCardSolver
+﻿namespace SetSolver
 {
     partial class ShapeCardSolver
     {
--- Set Solver/Form1.cs
+++ Set Solver/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ShapeCardSolver
+namespace SetSolver
 {
     public partial class ShapeCardSolver : Form
     {
